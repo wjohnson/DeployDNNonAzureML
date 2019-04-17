355326044a493e66e1bd273dd4bfb949655028c3
Will Johnson <wijohns@microsoft.com>
2019-04-17 09:17:00 -0500

    Task #158 is completed with this commit to create a workspace with a Service Principal that already exists and is a contributor to the resource group

--- mgmt/CreateWorkspace.py
+++ mgmt/CreateWorkspace.py
@@ -1,27 +1,50 @@
 from azureml.core import Workspace
 import os, json
 import azureml.core
-print("SDK Version:", azureml.core.VERSION)
-#print('current dir is ' +os.curdir)
-with open("aml_config/config.json") as f:
-    config = json.load(f)
+import azureml.core
 
-workspace_name = config['workspace_name']
-resource_group = config['resource_group']
-subscription_id = config['subscription_id']
-location = 'southcentralus'
+from azureml.core.runconfig import RunConfiguration
+from azureml.core import Workspace
+from azureml.core import ScriptRunConfig
+from azureml.core.authentication import ServicePrincipalAuthentication
+import os, json
+
+# Get workspace
+# ws = Workspace.from_config()
+svc_pr_password = os.environ.get("AZUREML_PASSWORD")
+tenant = os.environ.get("TENANT_ID")
+serviceprin = os.environ.get("APPID")
+sub = os.environ.get("SUBSCRIPTION")
+rg = os.environ.get("RESOURCE_GROUP")
+wrkspc = os.environ.get("WORKSPACE_NAME")
+wrkspc_loc = os.environ.get("WORKSPACE_LOCATION")
+
+
+# This Service Principal needs to have access to the resource group
+svc_pr = ServicePrincipalAuthentication(
+    tenant_id=tenant,
+    service_principal_id=serviceprin,
+    service_principal_password=svc_pr_password
+    )
+
+
 try:
-    ws = Workspace.get(name = workspace_name,
-                             subscription_id = subscription_id,
-                             resource_group = resource_group)
+    ws = Workspace.get(
+        subscription_id=sub,
+        resource_group=rg,
+        name=wrkspc,
+        auth=svc_pr
+    )
 
 except:
     # this call might take a minute or two.
-    ws = Workspace.create(name = workspace_name,
-                             subscription_id = subscription_id,
-                             resource_group = resource_group,
-                             #create_resource_group=True,
-                             location=location)
+    ws = Workspace.create(
+        subscription_id=sub,
+        resource_group=rg,
+        name=wrkspc,
+        auth=svc_pr,
+        location=wrkspc_loc
+    )
 
 # print Workspace details 
 print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep = '\n')
