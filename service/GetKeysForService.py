87f91776fe19ba5e435e4927919a45b661d998c1
Will Johnson <wijohns@microsoft.com>
2019-05-10 08:45:01 -0500

    Cleaned up the displaying of keys and URI for the just deployed service

--- service/GetKeysForService.py
+++ service/GetKeysForService.py
@@ -1,5 +1,4 @@
 from azureml.core import Workspace
-import azureml.core
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core.webservice import Webservice
 import os, json
@@ -11,23 +10,27 @@ serviceprin = os.environ.get("APPID")
 sub = os.environ.get("SUBSCRIPTION")
 rg = os.environ.get("RESOURCE_GROUP")
 wrkspc = os.environ.get("WORKSPACE_NAME")
-service_name = "aciwebservice042514"
 
-# This Service Principal needs to have access to the resource group
-svc_pr = ServicePrincipalAuthentication(
-    tenant_id=tenant,
-    service_principal_id=serviceprin,
-    service_principal_password=svc_pr_password
+if __name__ == "__main__":
+    # This Service Principal needs to have access to the resource group
+    svc_pr = ServicePrincipalAuthentication(
+        tenant_id=tenant,
+        service_principal_id=serviceprin,
+        service_principal_password=svc_pr_password
+        )
+    ws = Workspace.get(
+        subscription_id=sub,
+        resource_group=rg,
+        name=wrkspc,
+        auth=svc_pr
     )
-ws = Workspace.get(
-    subscription_id=sub,
-    resource_group=rg,
-    name=wrkspc,
-    auth=svc_pr
-)
-service= Webservice(name = service_name, workspace =ws)
+    with open('./script-outputs/aci_webservice.json', 'r') as fp:
+        config = json.load(fp)
+    service_name = config["aci_name"]
 
-# Get the ACI Details
-print("Scoring URI:{}".format(service.scoring_uri))
-print("Key:{}".format(service.get_keys()[0]))
+    service= Webservice(name = service_name, workspace =ws)
+
+    # Get the ACI Details
+    print("Scoring URI:{}".format(service.scoring_uri))
+    print("Key:{}".format(service.get_keys()[0]))
 
